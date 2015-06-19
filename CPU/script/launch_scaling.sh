ae3d8aa7828cb35731f535c38dd19631d9f039e5
Stefano Mandelli <linuxfree2@gmail.com>
2015-06-19 22:06:12 +0200

    renamed:    script/jobfile128 -> script/jobfile 	deleted:    script/jobfile256 	deleted:    script/jobfile32 	deleted:    script/jobfile64 	new file:   script/launch_scaling.sh 	deleted:    script/launch_scaling_128.sh 	deleted:    script/launch_scaling_256.sh 	deleted:    script/launch_scaling_32.sh 	deleted:    script/launch_scaling_64.sh

---
+++ CPU/script/launch_scaling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Variazione di beta
+
+STEP_MC=10000
+INIT=1
+SIZE=$1
+
+for k in $(<temp.dat);
+do
+	/home/$USER/Ising-Model/CPU/src/ising_CPU.x $SIZE $INIT $k $STEP_MC >> /home/$USER/Ising-Model/CPU/Ret_$SIZE.dat
+done
+
+
+exit 0;
