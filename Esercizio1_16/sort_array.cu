b21d57b0817d718bea904ece120d6b648a05c28b
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-21 18:13:14 +0100

    Ritorno sui propri passi
    ciclo iterativo a scalare di un fattore 16

--- Esercizio1_16/sort_array.cu
+++ Esercizio1_16/sort_array.cu
@@ -14,16 +14,14 @@ using namespace std;
 #define H_D cudaMemcpyHostToDevice
 #define D_H cudaMemcpyDeviceToHost
 
-#define L 516
+#define L 32
 #define BLOCKL 16
 
 
-__global__ void get_max(float *arr_num, float ABSMAX){
+__global__ void get_max(float *arr_num, float *sub_arr){
 	__shared__ float sub[BLOCKL];
-	__shared__ float subsub[L/BLOCKL];
 	sub[threadIdx.x] = arr_num[BLOCKL*blockIdx.x+threadIdx.x];
 	__shared__ float max;
-	ABSMAX=0;
 
 	max = sub[0];
 	for(int i=1; i<BLOCKL; ++i){
@@ -31,14 +29,8 @@ __global__ void get_max(float *arr_num, float ABSMAX){
 			max = sub[i];
 		}
 	}
-	subsub[blockIdx.x]=max;
+	sub_arr[blockIdx.x]=max;
 	__syncthreads();
-	
-	for(int k=0; k<L/BLOCKL; ++k){
-		if(&ABSMAX < subsub[k]){
-			&ABSMAX = subsub[k];
-		}
-	}
 }
 
 
@@ -46,11 +38,11 @@ __global__ void get_max(float *arr_num, float ABSMAX){
 
 
 int main(){
-	float *arr_num, *arr_num_d, *ABSMAX_d, *ABSMAX;
+	float *arr_num, *arr_num_d, *sub_arr, *sub_arr;
 	
 	//Dichiaro la mia array sulla CPU
 	arr_num=(float*)malloc(L*sizeof(float));
-	ABSMAX=(float*)malloc(sizeof(float));	
+	sub_arr=(float*)malloc((L/BLOCKL)*sizeof(float));	
 	//Riempio l'array con numeri random tra 0 e 10
 	for(int i=0; i<L; ++i){
 		arr_num[i]=10*(rand()/float(RAND_MAX));
@@ -63,10 +55,10 @@ int main(){
 
 	//Copio l'array sulla scheda grafica
 	cudaMalloc((void**)&arr_num_d, L*sizeof(float));
-	cudaMalloc((void**)&ABSMAX_d, sizeof(float));
+	cudaMalloc((void**)&sub_arr_d, (L/BLOCKL)*sizeof(float));
 	cudaMemcpy(arr_num_d, arr_num, L*sizeof(float), H_D);
 
-	get_max<<< grid , block >>>(arr_num_d,ABSMAX_d);
+	get_max<<< grid , block >>>(arr_num_d,sub_arr_d);
 
 
 
