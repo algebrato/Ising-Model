74e42e6231d2c76137ef3b9ff947aa4eacf48385
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-22 19:01:17 +0100

    Ruffini completato
    Regola di divisione di ruffini completata e funzionante

--- Esercizio1_16/Ruffini.m
+++ Esercizio1_16/Ruffini.m
@@ -1,16 +1,20 @@
 (** Ruffini **)
 
 RUF[x_, y_, t_] := Module[
-	  {coefx, coefy, MUL, n1, a},
-  coefx = CoefficientList[x, t]; 
-  coefy = CoefficientList[y, t];
-  n1 = -1*coefy[[1]];
-  For[
-	   i = 2, i < Length[coefx], ++i,
-   coefx[[i]] = coefx[[i]] + (coefx[[i - 1]]*n1)];
-  coefx
-  ]
+		  {coefx, coefy, MUL, n1, rest},
+   coefx = Reverse[CoefficientList[x, t]]; 
+   coefy = CoefficientList[y, t];
+   n1 = -1*coefy[[1]];
+   
+   For[
+		   i = 2, i <= Length[coefx], i++,
+   coefx[[i]] = coefx[[i]] + (coefx[[i - 1]]*n1)
+   ];
+   
+   rest = Last[coefx];
+   coefx=Delete[coefx,Length[coefx]];
 
-(**C'e` qualcosa che non funziona**)
+   Print[coefx,rest]
+  ]
 
 RUF[x^4 + 7 x + 6, x - 1, x]
